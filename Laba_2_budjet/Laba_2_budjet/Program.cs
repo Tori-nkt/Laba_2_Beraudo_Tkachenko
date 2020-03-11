41a52f757212aeb620f41137a475b654cef9a6cf
Tori-nkt <Tori-nkt@users.noreply.github.com>
2020-03-11 22:43:41 +0200

    Последний коммит
    Затупила и не сделала коммит после сдачи (во время сдачи ЛР мы внесли некоторые поправки по рекомендации преподователя)

--- Laba_2_budjet/Laba_2_budjet/Program.cs
+++ Laba_2_budjet/Laba_2_budjet/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Text;
+using System.Collections.Generic;
 
 namespace Laba_2_budjet
 {
@@ -13,43 +14,56 @@ namespace Laba_2_budjet
             int[,] bud = Rate_bud(budj);
             double[] sr = Sredniy_bal(bud, budj);
             double[] stepend = Sort_sp(sr);
-            Write_file();
+            Write_file(stepend, sr, budj);
+
         }
         public static int n, c = 0;
 
 
         public static string[] GetListStudents()
         {
+            
             Console.Write("Введіть назву теки: ");    // давайте употреблять малоизвестные слова
-            string path = Console.ReadLine();         // ну просто таааак
-            string[] list_bud = new string[30];       // простите, бомбит немножко
-            using (StreamReader sr = new StreamReader(path))
+            string storagePath = Console.ReadLine();
+            string[] list_bud = new string[60];       // простите, бомбит немножко
+            
+            var fileStorage = new List<string[]>();
+            try
             {
-                string line;
-                int count = 0;
-                int n = Convert.ToInt32(sr.ReadLine());
-                while (count != n)
+                var filePaths = Directory.GetFiles(storagePath, "*.csv"); //string[]
+                foreach (string path in filePaths)
                 {
-                    //students1.csv
-                    line = sr.ReadLine();
-                    if (line.EndsWith("FALSE"))
+                    string[] fileLines = System.IO.File.ReadAllLines(path);
+                    fileStorage.Add(fileLines);
+                }
+                foreach (string[] fileLines in fileStorage)
+                {
+                    foreach (string fileLine in fileLines)
                     {
-                        list_bud[c] = line;
-                        c++;
+                        if (fileLine.EndsWith("TRUE"))
+                        {
+                            list_bud[c] = fileLine;
+                            c++;
+                        }
                     }
-                    count++;
+                    Console.WriteLine();
                 }
             }
-            return list_bud;
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+            return list_bud;//создаём список бюджетников            
         }
-        static int[,] Rate_bud(string[] budj)              //создаём список бюджетников
+        static int[,] Rate_bud(string[] budj)       //создаём список оценок студентов       
         {
             int[,] sp_budj = new int[c, 5];
             for (int i = 0; i < c; i++)
             {
                 string line = budj[i];
                 string[] temp = line.Split(",");
-                if (line.EndsWith("FALSE"))    // на счёт false не уверенна, но суть не меняется
+                if (line.EndsWith("TRUE"))    // на счёт false не уверенна, но суть не меняется
                 {
                     Console.WriteLine(line);
                     for (int j = 0; j < 5; j++)
@@ -76,19 +90,19 @@ namespace Laba_2_budjet
                 }
                 sredniy[i] = sum / 5;
                 Console.Write("{0} ", tem[0]);      // для наглядности го попринтим)
-                Console.WriteLine("{0:f3}", sredniy[i]);
+                Console.WriteLine("{0:f3}", sredniy[i]);  // {0:f3} форматированый вывод double (3 знака после запятой)
             }
             return sredniy;
         }
         static int Find_Percent(int k)
         {
-            int percent = c * k / 100;
+            int percent = c * k / 100;   // с - количество бюджетников, k - процент которій мы хотим узнать
             return percent;
         }
         static double[] Sort_sp(double[] sp)    //находим тех, кто получает стипендию
         {
             double temp;
-            for (int i = 0; i < c; i++)         // сортируем список сред. баллов
+            for (int i = 0; i < c; i++)         // сортируем список сред. баллов, по спаданию
             {
                 for (int j = i + 1; j < 5; j++)
                 {
@@ -109,17 +123,17 @@ namespace Laba_2_budjet
             
             return stependia;
         }
-        static void Write_file()
+        static void Write_file(double[] step, double[] sred_b, string[] bud)
         {
             Console.Write("Введіть назву теки для запису: ");
             string path = Console.ReadLine();
             try
             {
-                // создание файла
+                               // создание файла
                 using (FileStream fs = File.Create(path))
                 {
-                    byte[] info = new UTF8Encoding(true).GetBytes("Список бюджетников");
-                    // Add some information
+                    byte[] info = new UTF8Encoding(true).GetBytes("Список бюджетников в формате (имя) (средний бал) (на стипендии)");
+                              // Add some information
                     fs.Write(info, 0, info.Length);
                 }
             }
@@ -127,6 +141,44 @@ namespace Laba_2_budjet
             {
                 Console.WriteLine(ex.ToString());
             }
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(path))
+                {
+                    int per = Find_Percent(40);
+                    for (int i = 0; i < c; i++)
+                    {
+                        string[] temp = (bud[i]).Split(",");
+                        sw.Write(temp[0]);
+                        sw.Write("  ср. балл: {0}  ", sred_b[i]);
+                        for (int j = 0; j < per; j++)
+                        {
+                            if (sred_b[i] == step[j])
+                            {
+                                sw.Write(" На стипендии");
+                                break;
+                            }
+                        }
+                        sw.WriteLine();
+                    }
+                }
+                using (StreamReader sr = new StreamReader(path))
+                {
+                    string line;
+                    int count = 0;
+                    while (count != c + 1)
+                    {
+                        line = sr.ReadLine();
+                        Console.WriteLine(line);
+                        count++;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
         }
     }
 }
